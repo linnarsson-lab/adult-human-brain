b67976f01260c0d419fc82f8861f42276a345518
Gioele La Manno <gioelelamanno@gmail.com>
2016-11-07 11:27:20 +0100

    logging with different verbosity levels

--- diffusion_topology/bi_pca.py
+++ diffusion_topology/bi_pca.py
@@ -5,6 +5,7 @@ from scipy.special import polygamma
 from scipy.stats import mannwhitneyu
 from statsmodels.sandbox.stats.multicomp import multipletests
 import numpy as np
+import logging
 
 def broken_stick(n, k):
 	"""
@@ -20,7 +21,7 @@ def broken_stick(n, k):
 	"""
 	return np.array( [((polygamma(0,1+n)-polygamma(0,x+1))/n) for x in range(k)] )
 
-def biPCA(data, n_splits=10, n_components=20, cell_limit=10000, smallest_cluster = 5):
+def biPCA(data, n_splits=10, n_components=20, cell_limit=10000, smallest_cluster = 5, verbose=2):
 	'''biPCA algorythm for clustering using PCA splits
 
 	Args
@@ -34,18 +35,25 @@ def biPCA(data, n_splits=10, n_components=20, cell_limit=10000, smallest_cluster
 		cell_limit : int
 			the max number of cells that are used to calculate PCA, if #cells>cell_limit, 
 			a random sample of cell_limit cells will be drawn
+		level_of_verbosity: int
+			0 for only ERRORS
+			1 for WARNINGS and ERRORS
+			2 for INFOS, WARNINGS and ERRORS
+			3 for DEBUGGING, NFOS, WARNINGS and ERRORS
 
 	Returns
 	-------
 
 	'''
 
+	logging.basicConfig(format='%(message)s', level=[logging.ERROR,logging.WARNING,logging.INFO,logging.DEBUG][verbose])
+
 	n_genes, n_cells = data.shape
 	cell_labels_by_depth = np.zeros((n_splits+1, n_cells))
 	
 	# Run a iteration per level of depth
 	for i_split in range(n_splits):
-		print "Depth: %i" % i_split
+		logging.info( "Depth: %i" % i_split )
 		running_label_id = 0
 		parent_clusters = np.unique(cell_labels_by_depth[i_split, :])
 		# Consider every parent cluster and split them one by one
@@ -67,13 +75,15 @@ def biPCA(data, n_splits=10, n_components=20, cell_limit=10000, smallest_cluster
 			bs = broken_stick(n_genes, min(n_components, len(current_cells_ixs) ))
 			sig = pca.explained_variance_ratio_ > bs
 			
+			# No principal component is significant, don't split
 			if not np.any(sig):
-				# No principal component is significant, don't try to split
-				print "No principal component is significant, don't try to split"
+				
+				logging.debug( "No principal component is significant, don't split!" )
 				cell_labels_by_depth[i+1, current_cells_ixs] = running_label_id
 				running_label_id += 1
 				continue
-			
+			logging.debug('%i principal components are significant' % sum(sig))
+
 			if sum(sig) < n_components:
 				# Take only the significant components
 				first_non_sign = np.where(np.logical_not(sig))[0][0]
@@ -104,34 +114,37 @@ def biPCA(data, n_splits=10, n_components=20, cell_limit=10000, smallest_cluster
 				if score > best_score:
 					best_score = score
 					best_labels = labels
-			print "Proposed split (%i,%i) has best_score: %s" % (sum(best_labels==0),sum(best_labels==1), best_score)
+			logging.debug("Proposed split (%i,%i) has best_score: %s" % (sum(best_labels==0),sum(best_labels==1), best_score) )
 			ids, cluster_counts = np.unique(best_labels, return_counts=True)			
 			
 			# Check that no small cluster gets generated
 			if min(cluster_counts) < smallest_cluster:
 				# Reject split immediatelly and continue
-				print ids, cluster_counts
-				print "A small cluster get generated, don't split'"
+				logging.debug( "A small cluster get generated, don't split'" )
 				cell_labels_by_depth[i_split+1, current_cells_ixs] = running_label_id
 				running_label_id += 1
 				continue
-
+			
+			# Conside only the 500 genes with top loadings
 			sum_loadings_per_gene =  np.abs(  pca.components_.T[:,:first_non_sign].sum(1) )
-			topload_gene_ixs = np.argsort(sum_loadings_per_gene)[::-1][:500] # max top 500 genes are used 
+			topload_gene_ixs = np.argsort(sum_loadings_per_gene)[::-1][:500]
 
 			# Here I use mannwhitney U instead of binomial test
 			p_values = np.array([ test_gene(data, current_cells_ixs, i, best_labels, 0) for i in topload_gene_ixs])
 			p_values = np.minimum(p_values, 1-p_values)
 			rejected_null, q_values = multipletests(p_values, 0.05, 'fdr_bh')[:2]
+
 			# Decide if we should accept the split
-			if (np.sum(rejected_null) > 0) and (best_score>0.01):
+			# Here instead of arbitrary threshold on shilouette I could boostrap
+			if (np.sum(rejected_null) > 5) and (best_score>0.01):
 				# Accept
+				logging.debug("Spltting with significant genes: %s; silhouette-score: %s" % (np.sum(rejected_null), best_score))
 				cell_labels_by_depth[i_split+1, current_cells_ixs[best_labels == 0]] = running_label_id 
 				cell_labels_by_depth[i_split+1, current_cells_ixs[best_labels == 1]] = running_label_id + 1
 				running_label_id += 2
 			else:
 				# Reject
-				print "Don't split becouse' becouse chekc parameters are: %s, %s" % (np.sum(rejected_null), best_score)
+				logging.debug( "Don't split. Significant genes: %s (min=5); silhouette-score: %s(min=0.01)" % (np.sum(rejected_null), best_score) )
 				cell_labels_by_depth[i_split+1, current_cells_ixs] = running_label_id
 				running_label_id += 1
 	return cell_labels_by_depth
