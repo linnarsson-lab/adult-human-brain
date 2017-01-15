8e88b91b746c90a5318f89ca040b5f3a923cdc12
Gioele La Manno <gioelelamanno@gmail.com>
2017-01-15 18:50:36 +0100

    make this functions available when importing

--- cytograph/__init__.py
+++ cytograph/__init__.py
@@ -3,7 +3,7 @@ from .preprocessing import preprocess
 from .bi_pca import broken_stick, biPCA, select_sig_pcs
 from .diff_exp import expression_patterns, betabinomial_trinarize_array
 from .auto_annotator import AutoAnnotator
-from .cytograph import Cytograph, plot_clusters, pca_projection, feature_selection, Normalizer
+from .cytograph import Cytograph, plot_clusters, pca_projection, feature_selection, Normalizer, plot_clusters, save_auto_annotation, save_diff_expr
 from .prommt import ProMMT
 from .facet_learning import Facet, FacetLearning
 from .louvain_jaccard import LouvainJaccard
