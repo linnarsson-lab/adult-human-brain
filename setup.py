00ca014515fc0bd23f5407f8c5bea304b6d67b52
Sten Linnarsson <sten.linnarsson@ki.se>
2017-02-18 16:55:19 +0100

    fix installation of numpy_groupies

--- setup.py
+++ setup.py
@@ -4,27 +4,28 @@ __version__ = "0.0.0"
 exec(open('cytograph/_version.py').read())
 
 setup(
-    name="cytograph",
-    version=__version__,
-    packages=find_packages(),
-    install_requires=[
-        'loompy',
-        'numpy',
-        'scikit-learn',
-        'scipy',
-        'numpy_groupies',
-        'networkx',
-        'palettable',
-        'tqdm',
-        'python-louvain',
-        'luigi'
-    ],
-    # loom command
-    scripts=['cytograph/markerheatmap'],
-    # metadata
-    author="Linnarsson Lab",
-    author_email="sten.linnarsson@ki.se",
-    description="Pipeline for single-cell RNA-seq analysis",
-    license="MIT",
-    url="https://github.com/linnarsson-lab/cytograph",
+	name="cytograph",
+	version=__version__,
+	packages=find_packages(),
+	install_requires=[
+		'loompy',
+		'numpy',
+		'scikit-learn',
+		'scipy',
+		'networkx',
+		'palettable',
+		'tqdm',
+		'python-louvain',
+		'luigi'
+	],
+	# commands
+	dependency_links=['http://github.com/linnarsson-lab/numpy-groupies/tarball/master#egg=numpy_groupies'],
+	scripts=['cytograph/markerheatmap'],
+	
+	# metadata
+	author="Linnarsson Lab",
+	author_email="sten.linnarsson@ki.se",
+	description="Pipeline for single-cell RNA-seq analysis",
+	license="MIT",
+	url="https://github.com/linnarsson-lab/cytograph",
 )
