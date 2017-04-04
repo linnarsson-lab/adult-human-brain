396068e84f44d3bff2759a6cde58a2413510008d
Gioele La Manno <gioelelamanno@gmail.com>
2017-04-04 15:43:43 +0200

    fix bug in the __init__

--- cytograph/luigi/development/__init__.py
+++ cytograph/luigi/development/__init__.py
@@ -3,8 +3,6 @@ from .cluster_layout_dev import ClusterLayoutDev
 from .auto_annotate_dev import AutoAnnotateDev
 from .plot_graph_dev import PlotGraphDev
 from .trinarize_dev import TrinarizeDev
-from .lineage import AutoAnnotateLineage, MarkerEnrichmentLineage, ExpressionAverageLineage, TrinarizeLineage
+from .lineage import MarkerEnrichmentLineage, ExpressionAverageLineage, LineageAnalysis
 from .lineage import PlotCVMeanLineage, PlotGraphAgeLineage, PlotClassesLineage
-from .lineage import LineageAnalysis
-from .time import ExpressionAverageTime
-from .time import TimeAnalysis
+from .time import ExpressionAverageTime, TimeAnalysis
