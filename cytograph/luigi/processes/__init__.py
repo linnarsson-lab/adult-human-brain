4aecc8843fd886857a5edbb967416ab731aaf808
Gioele La Manno <gioelelamanno@gmail.com>
2017-04-30 17:09:22 +0200

    added new tasks

--- cytograph/luigi/processes/__init__.py
+++ cytograph/luigi/processes/__init__.py
@@ -2,6 +2,10 @@ from .studyprocess_pool import StudyProcessPool
 from .studyprocess import StudyProcess
 from .auto_annotate_process import AutoAnnotateProcess
 from .cluster_layout_process import ClusterLayoutProcess
+from .expression_average_process import ExpressionAverageProcess
+from .expression_average_time_process import ExpressionAverageTimeProcess
+from .marker_enrichment_process import MarkerEnrichmentProcess
+from .plot_cv_mean_process import PlotCVMeanProcess
 from .plot_graph_age_process import PlotGraphAgeProcess
 from .plot_graph_process import PlotGraphProcess
 from .trinarize_process import TrinarizeProcess
