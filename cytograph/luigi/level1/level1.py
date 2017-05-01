54117528578bb85b5743723b63f8a220d6757d50
Gioele La Manno <gioelelamanno@gmail.com>
2017-05-01 16:55:42 +0200

    bug4

--- cytograph/luigi/level1/level1.py
+++ cytograph/luigi/level1/level1.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cytograph as cg
 import luigi
-from .. import time_check, targets_map
 
 
 class Level1(luigi.WrapperTask):
@@ -20,7 +19,7 @@ class Level1(luigi.WrapperTask):
 	time = luigi.Parameter(default="E7-E18")  # later more specific autoannotation can be devised
 
 	def requires(self) -> Iterator[luigi.Task]:
-		tissues = targets_map[self.target]
+		tissues = cg.targets_map[self.target]
 		for tissue in tissues:
-			if time_check(tissue, self.time):
+			if cg.time_check(tissue, self.time):
 				yield cg.ClusterLayoutL1(tissue=tissue), cg.AutoAnnotateL1(tissue=tissue), cg.PlotGraphL1(tissue=tissue)
