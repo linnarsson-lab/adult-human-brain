38112e51d14b58404e36fec349112ed8952d4aa4
Gioele La Manno <gioelelamanno@gmail.com>
2017-05-11 18:26:20 +0200

    same as before many times

--- cytograph/luigi/adolescent_L2/__init__.py
+++ cytograph/luigi/adolescent_L2/__init__.py
@@ -2,10 +2,7 @@ from .auto_annotate_L2 import AutoAnnotateL2
 from .manifold_L2 import ManifoldL2
 from .cluster_L2 import ClusterL2
 from .level2_adolescent import Level2Adolescent
-from .plot_cv_mean_L2 import PlotCVMeanL2
 from .plot_graph_L2 import PlotGraphL2
 from .split_and_pool import SplitAndPool
-from .trinarize_L2 import TrinarizeL2
-from .plot_classes_L2 import PlotClassesL2
 from .plot_markerheatmap_L2 import PlotMarkerheatmapL2
 from .aggregate_L2 import AggregateL2
