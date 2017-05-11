335026cf20636a2edf2fb853d4ccd955d747883b
Gioele La Manno <gioelelamanno@gmail.com>
2017-05-11 18:24:58 +0200

    Sten is importing a module that does not exist

--- cytograph/luigi/adolescent_L2/__init__.py
+++ cytograph/luigi/adolescent_L2/__init__.py
@@ -1,9 +1,7 @@
-
 from .auto_annotate_L2 import AutoAnnotateL2
 from .manifold_L2 import ManifoldL2
 from .cluster_L2 import ClusterL2
 from .level2_adolescent import Level2Adolescent
-from .marker_enrichment_L2 import MarkerEnrichmentL2
 from .plot_cv_mean_L2 import PlotCVMeanL2
 from .plot_graph_L2 import PlotGraphL2
 from .split_and_pool import SplitAndPool
