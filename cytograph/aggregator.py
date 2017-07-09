41fc8a32a1f82c098afb29528fc59321c7209b7c
Sten Linnarsson <sten.linnarsson@ki.se>
2017-07-09 21:11:31 +0200

    keep outliers separate

--- cytograph/aggregator.py
+++ cytograph/aggregator.py
@@ -45,6 +45,13 @@ class Aggregator:
         D = pdist(data, 'euclidean')
         Z = hc.linkage(D, 'ward')
         merged = hc.fcluster(Z, 5, criterion='distance') - 1
+
+        # Keep the outliers separate
+        outliers = merged[dsout.col_attrs["Outliers"] == 1][0]
+        if (merged == outliers).sum() > 1:
+            merged[dsout.col_attrs["Outliers"] == 1] = -1
+            merged += 1
+
         new_clusters = renumber(ds.col_attrs["Clusters"], np.arange(n_labels), merged)
         ds.set_attr("Clusters", new_clusters, axis=1)
         dsout.close()
