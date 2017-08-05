3403022b92c292a2dbc948a6b0a22cb026e3f620
Gioele La Manno <gioelelamanno@gmail.com>
2017-08-05 23:53:21 +0200

    revert back to preoptimization with indexing trick

--- cytograph/HPF.py
+++ cytograph/HPF.py
@@ -174,7 +174,7 @@ class HPF:
             # phi is calculated on log scale from expectations of the gammas, hence the digamma and log terms
             # Shape of phi will be (nnz, k)
             # clock.tic()
-            phi = numexpr_digamma(gamma_shape)[u, :] + numexpr_digamma(lambda_shape)[i, :] - fast_logprod(gamma_rate[u, :], lambda_rate[i, :])
+            phi = (digamma(gamma_shape) - np.log(gamma_rate))[u, :] + (digamma(lambda_shape) - np.log(lambda_rate))[i, :]
             # logging.debug("phi_calc %.4e" % clock.toc())
             # Multiply y by phi normalized (in log space) along the k axis
             # clock.tic()
