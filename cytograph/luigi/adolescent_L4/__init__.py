793ed19f05c86350173b6cfa68a0e216b1df4b37
Sten Linnarsson <sten.linnarsson@ki.se>
2017-08-15 18:03:59 +0200

    x

--- cytograph/luigi/adolescent_L4/__init__.py
+++ cytograph/luigi/adolescent_L4/__init__.py
@@ -1,4 +1,4 @@
 from .export_L4 import ExportL4
 from .aggregate_L4 import AggregateL4
-from .cluster_L4 import ClusterL4
+from .pool_L4 import PoolL4
 from .level1234_adolescent import Level1234Adolescent
