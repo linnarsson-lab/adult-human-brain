3b6ad73433d15659a71a82aeb9f257f9fe4b0dae
Sten Linnarsson <sten.linnarsson@ki.se>
2019-05-07 10:37:53 +0200

    buuug

--- cytograph/pipeline/cytograph.py
+++ cytograph/pipeline/cytograph.py
@@ -123,18 +123,18 @@ class Cytograph:
 		start = 0
 		batch_size = 6400
 		if "velocity" in self.steps and "spliced" in ds.layers:
-			beta = ds.ra.HPF_beta  # The unnormalized beta
+			beta_all = ds.ra.HPF_beta  # The unnormalized beta
 			ds["spliced_exp"] = 'float32'
 			ds['unspliced_exp'] = 'float32'
 		while start < n_samples:
 			# Compute PPV (using normalized theta)
-			ds["expected"][:, start: start + batch_size] = beta @ theta[start: start + batch_size, :].T
+			ds["expected"][:, start: start + batch_size] = beta_all @ theta[start: start + batch_size, :].T
 			# Compute PPV using raw theta, for calculating posterior probability of the observations
 			ppv_unnormalized = beta @ theta_unnormalized[start: start + batch_size, :].T
 			log_posterior_proba[start: start + batch_size] = poisson.logpmf(data.T[:, start: start + batch_size], ppv_unnormalized).sum(axis=0)
 			if "velocity" in self.steps and "spliced" in ds.layers:
-				ds["spliced_exp"][:, start: start + batch_size] = beta @ theta_spliced[start: start + batch_size, :].T
-				ds["unspliced_exp"][:, start: start + batch_size] = beta @ theta_unspliced[start: start + batch_size, :].T
+				ds["spliced_exp"][:, start: start + batch_size] = beta_all @ theta_spliced[start: start + batch_size, :].T
+				ds["unspliced_exp"][:, start: start + batch_size] = beta_all @ theta_unspliced[start: start + batch_size, :].T
 			start += batch_size
 		ds.ca.HPF_LogPP = log_posterior_proba
 
