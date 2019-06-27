712f7042538b0c2d043009ff57e4ed0d56ec7d51
Sten Linnarsson <sten.linnarsson@ki.se>
2019-06-27 19:48:48 +0300

    unnecessary import

--- cytograph/pipeline/__init__.py
+++ cytograph/pipeline/__init__.py
@@ -3,4 +3,3 @@ from .config import config
 from .cytograph import Cytograph
 from .punchcards import Punchcard, PunchcardSubset, PunchcardDeck
 from .workflow import RootWorkflow, SubsetWorkflow, PoolWorkflow
-#from .commands import cli
