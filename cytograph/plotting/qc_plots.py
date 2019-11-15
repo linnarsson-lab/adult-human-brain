5237a07b555851b0ca88f8e68ce0187022cf050e
miri <miri@monod.mbb.ki.se>
2019-11-15 10:24:25 +0100

    Added mitochondrial genes ratio and DoubletFinder score to the QC plot + randomize the cells presentation on the TSNE

--- cytograph/plotting/qc_plots.py
+++ cytograph/plotting/qc_plots.py
@@ -8,13 +8,13 @@ from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 
 def all_QC_plots(ds: loompy.LoomConnection=None,out_file: str="Tmp.png" ):
-    f, ax = plt.subplots(3, 2,  figsize=(20,20))
+    f, ax = plt.subplots(4, 2,  figsize=(2*8,4*8))
     
     plot_title = "Mitochondrial gene expression ratio distribution"
     dist_attr(ax[0,0],ds,  attr = "MT_ratio", plot_title= plot_title, line=0.05)
     plot_title = "Unspliced reads ratio distribution"
     dist_attr(ax[0,1],ds,  attr = "unspliced_ratio", plot_title= plot_title, line=0.5)
-    attrs_on_TSNE(ax[1:3,0:2],ds ,attrs = ["DoubletFinderFlag","TotalUMI","unspliced_ratio","NGenes"],plot_title = ["Doublets Flag","UMI counts","Unspliced UMI ratio","Number of genes per cell"])
+    attrs_on_TSNE(ax[1:4,0:2],ds ,attrs = ["DoubletFinderFlag","DoubletFinderScore","TotalUMI","NGenes","unspliced_ratio","MT_ratio"],plot_title = ["Doublets Flag","Doublets Score","UMI counts","Number of genes per cell","Unspliced UMI ratio","Mitochondrial genes ratio"])
     
     f.savefig(out_file, dpi=144)
 
@@ -39,8 +39,10 @@ def attrs_on_TSNE(ax:plt.axes = None,ds: loompy.LoomConnection=None, out_file: s
     n_cols = 2
     n_rows = np.ceil(n_attr/n_cols)
     n_rows = n_rows.astype(int)
-    if ax is None:
-        fig,ax = plt.subplots(n_rows,n_cols,figsize=(12,12))
+    if ax is None and n_attr>1:
+        fig,ax = plt.subplots(n_rows,n_cols,figsize=(8*n_cols,8*n_rows))
+    elif  ax is None and n_attr == 1:
+        fig,ax = plt.subplots(figsize=(12,12))
     n_cols_plot = 0
     n_rows_plot = 0
     if 'TSNE' in ds.ca:
@@ -72,14 +74,15 @@ def attrs_on_TSNE(ax:plt.axes = None,ds: loompy.LoomConnection=None, out_file: s
         #ax.append( plt.subplot(n_rows,n_cols,n+1))
         if(n_attr==1):
             current_ax = ax
-        if(n_attr>1):
+        if(n_attr==2):
+            current_ax = ax[n_cols_plot]
+        if(n_attr>2):
             current_ax = ax[n_rows_plot,n_cols_plot]
-        if attr in ds.ca:
-
-             
+        if attr in ds.ca: 
             current_ax.scatter(xy[:, 0], xy[:, 1], c='lightgrey', lw=0, s=10)
-            labels = ds.ca[attr]
-            if (len(np.unique(labels))>20):
+            names,labels = np.unique(ds.ca[attr],return_inverse=True)
+            if (len(names)>20):
+                labels  = ds.ca[attr]
                 if(np.max(ds.ca[attr])>10000):
                     labels = np.log(labels)
                     if plot_title is not None:
@@ -89,20 +92,16 @@ def attrs_on_TSNE(ax:plt.axes = None,ds: loompy.LoomConnection=None, out_file: s
                 cm = plt.cm.get_cmap('jet')
                 cells = ds.ca[attr] > 0
                 sc = current_ax.scatter(xy[:, 0][cells], xy[:, 1][cells], c=labels[cells], lw=0, s=10,cmap = cm)
-                plt.colorbar(sc,ax=ax[n_rows_plot,n_cols_plot])
+                plt.colorbar(sc,ax=current_ax)
             else:
-                for lbl in np.unique(labels):
-                    cells = labels == lbl
-                    current_ax.scatter(xy[:, 0][cells], xy[:, 1][cells], c=colorize(labels)[cells], label=lbl, lw=0, s=0)
-                    cells = np.random.permutation(labels.shape[0])
-                    current_ax.scatter(xy[:, 0][cells], xy[:, 1][cells], c=colorize(labels)[cells], lw=0, s=10)
-                    lgnd = current_ax.legend()
-                    if plot_title is not None:
-                    #Add min, max
-                        current_ax.set_title(plot_title[n])
-                    for handle in lgnd.legendHandles:
-                        handle.set_sizes([10])
-
+                colors = colorize(names)
+                cells = np.random.permutation(labels.shape[0])
+                current_ax.scatter(xy[cells, 0], xy[cells, 1], c=colors[labels][cells], lw=0, s=10)
+                h = lambda c: plt.Line2D([], [], color=c, ls="", marker="o")
+                current_ax.legend(handles=[h(colors[i]) for i in range(len(names))],labels=list(names),loc='lower left',
+		        markerscale=1,frameon=False,fontsize=10)
+                if plot_title is not None:
+                    current_ax.set_title(plot_title[n])
         else:
             current_ax.scatter(xy[:, 0], xy[:, 1], c='lightgrey', lw=0, s=10)
             if plot_title is not None:
@@ -114,7 +113,6 @@ def attrs_on_TSNE(ax:plt.axes = None,ds: loompy.LoomConnection=None, out_file: s
             n_rows_plot = n_rows_plot+1
     if out_file is not None: 
         fig.savefig(out_file, dpi=144)
-    #fig.close()
     return(ax)       
             
            
