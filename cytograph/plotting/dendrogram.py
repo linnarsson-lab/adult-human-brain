61dd57920bb72182dfb509eef2b3ccb4f7b6729f
Sten Linnarsson <stelin@retzius.local>
2019-12-27 21:29:01 +0100

    implement "bottom" orientation

--- cytograph/plotting/dendrogram.py
+++ cytograph/plotting/dendrogram.py
@@ -1,8 +1,9 @@
 import numpy as np
+from typing import Tuple
 from matplotlib.collections import LineCollection
 
 
-def dendrogram(z: np.ndarray, *, leaf_positions: np.ndarray = None, orientation: str = "top") -> LineCollection:
+def dendrogram(z: np.ndarray, *, leaf_positions: np.ndarray = None, orientation: str = "top", skip_top: int = 0) -> LineCollection:
 	"""
 	Create a dendrogram, in the form of a matplotlib LineCollection, in data space, and optionally
 	with unevenly spaced leaves given by leaf_positions.
@@ -21,10 +22,8 @@ def dendrogram(z: np.ndarray, *, leaf_positions: np.ndarray = None, orientation:
 	# are combined to form cluster (n + i). A cluster with an index less than n corresponds to one of the n
 	# original observations. The distance between clusters Z[i, 0] and Z[i, 1] is given by Z[i, 2]. The
 	# fourth value Z[i, 3] represents the number of original observations in the newly formed cluster.
-
-	for i in range(z.shape[0]):
-		# Draw a Π-shaped glyph with the left leg extending to the top of cluster z[i, 0] and the right leg
-		# to cluster z[i, 1] and the crossbar at level z[i, 2].
+	zmax = np.max(z[:, 2])
+	for i in range(z.shape[0] - skip_top):
 		if z[i, 0] < n:
 			left_y = 0
 			left_x = leaf_positions[int(z[i, 0])]
@@ -40,7 +39,13 @@ def dendrogram(z: np.ndarray, *, leaf_positions: np.ndarray = None, orientation:
 		xpos[i] = left_x + (right_x - left_x) / 2
 		crossbar_y = z[i, 2]
 		if orientation == "top":
+			# Draw a Π-shaped glyph with the left leg extending to the top of cluster z[i, 0] and the right leg
+			# to cluster z[i, 1] and the crossbar at level z[i, 2].
 			lines.append([[left_x, left_y], [left_x, crossbar_y], [right_x, crossbar_y], [right_x, right_y]])
+		elif orientation == "bottom":
+			# Draw a ⨆ -shaped glyph with the left leg extending to the bottom of cluster z[i, 0] and the right leg
+			# to cluster z[i, 1] and the crossbar at level -z[i, 2].
+			lines.append([[left_x, zmax - left_y], [left_x, zmax - crossbar_y], [right_x, zmax - crossbar_y], [right_x, zmax - right_y]])
 		else:
-			raise NotImplementedError("Only 'top' orientation is supported")
+			raise NotImplementedError("Only 'top' or 'bottom' orientation is supported")
 	return LineCollection(lines, colors="black")
