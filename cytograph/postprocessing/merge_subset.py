241f5f5a435018e8bfb50c0a62675d1efd20fdc6
kasiletti <kasiletti@gmail.com>
2022-01-25 11:34:48 +0100

    changes to merge function

--- cytograph/postprocessing/merge_subset.py
+++ cytograph/postprocessing/merge_subset.py
@@ -10,7 +10,7 @@ from ..species import Species
 from ..enrichment import FeatureSelectionByEnrichment
 
 
-def merge_subset(subset: str, config, threshold: int = 10) -> None:
+def merge_subset(subset: str, config, threshold: int = 5) -> None:
 
     loom_file = os.path.join(config.paths.build, "data", subset + ".loom")
 
@@ -19,7 +19,7 @@ def merge_subset(subset: str, config, threshold: int = 10) -> None:
         # Copy original clusters
         ds.ca.ClustersPremerge = np.copy(ds.ca.Clusters)
         logging.info(f'Starting with {ds.shape[1]} cells in {ds.ca.Clusters.max() + 1} clusters')
-        logging.info(f'Threshold: {threshold} enriched genes with qval < 0.001')
+        logging.info(f'Threshold: {threshold} enriched genes with qval < 0.001, enrichment > 5')
 
         merge_flag = True
 
@@ -38,14 +38,14 @@ def merge_subset(subset: str, config, threshold: int = 10) -> None:
             features = FeatureSelectionByEnrichment(1, Species.mask(ds, config.params.mask), findq=True)
             features.fit(ds)
             # Count statistically enriched genes for each cluster
-            scores = np.count_nonzero(features.qvals < 0.001, axis=0)
+            scores = np.count_nonzero((features.qvals < 0.001) & (features.enrichment > 5), axis=0)
             scores = np.array(scores)
             logging.info(scores)
 
             # If more than one cluster has fewer enriched genes than threshold
             if (scores < threshold).sum() > 1:
-                # Calculate cluster distances on the UMAP
-                mu = npg.aggregate(clusters, ds.ca.UMAP.T, func='mean', axis=1, fill_value=0)
+                # Calculate cluster distances on the TSNE
+                mu = npg.aggregate(clusters, ds.ca.TSNE.T, func=lambda x: np.median(x), axis=1, fill_value=0)
                 D = squareform(pdist(mu.T))
                 np.fill_diagonal(D, np.inf)
                 # Find cluster with the minimum score
@@ -64,12 +64,14 @@ def merge_subset(subset: str, config, threshold: int = 10) -> None:
         exportdir = os.path.join(config.paths.build, "merge", "plots")
         if not os.path.exists(exportdir):
             os.mkdir(exportdir)
-        plt.figure(None, (15, 5))
-        plt.subplot(131)
-        plt.scatter(ds.ca.UMAP[:, 0], ds.ca.UMAP[:, 1], c=colorize(ds.ca.ClustersPremerge), s=5)
+        plt.figure(None, (10, 5))
+        plt.subplot(121)
+        plt.scatter(ds.ca.TSNE[:, 0], ds.ca.TSNE[:, 1], c=colorize(ds.ca.ClustersPremerge), s=5)
         plt.title('Pre-merge clusters')
-        plt.subplot(132)
-        plt.scatter(ds.ca.UMAP[:, 0], ds.ca.UMAP[:, 1], c=colorize(ds.ca.Clusters), s=5)
+        plt.axis('off')
+        plt.subplot(122)
+        plt.scatter(ds.ca.TSNE[:, 0], ds.ca.TSNE[:, 1], c=colorize(ds.ca.Clusters), s=5)
         plt.title('Post-merge clusters')
+        plt.axis('off')
         plt.savefig(os.path.join(exportdir, f'{subset}.png'), dpi=150)
         plt.close()
