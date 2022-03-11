c2e16a2c7fceda7ccf6ef099c5205441a447a167
kasiletti <kasiletti@gmail.com>
2022-03-11 15:05:03 +0100

    mito_genes_ratio handles MT and chrM

--- cytograph/preprocessing/qc_functions.py
+++ cytograph/preprocessing/qc_functions.py
@@ -4,11 +4,13 @@ import loompy
 import os
 
 def mito_genes_ratio(ds: loompy.LoomConnection) -> None:
-    mito_genes = np.where(ds.ra.Chromosome=="chrM")[0]
-    exp_mito = ds[mito_genes,:]
+    mito_genes = ds.ra.Chromosome == "MT"
+    if mito_genes.sum() == 0:
+        mito_genes = ds.ra.Chromosome == "chrM"
+    exp_mito = ds[mito_genes, :]
     sum_mito = exp_mito.sum(axis=0)
     sum_all = ds.ca.TotalUMI
-    mito_ratio = np.divide(sum_mito,sum_all)
+    mito_ratio = np.divide(sum_mito, sum_all)
     ds.ca["MT_ratio"] = mito_ratio
     
         
@@ -16,7 +18,7 @@ def unspliced_ratio(ds: loompy.LoomConnection, graphs: bool = True, sample_name 
     u = ds.layers["unspliced"][:]
     sum_all = ds.ca.TotalUMI
     sum_u = u.sum(axis=0)
-    unspliced_ratio = np.divide(sum_u,sum_all)
+    unspliced_ratio = np.divide(sum_u, sum_all)
     ds.ca["unspliced_ratio"] = unspliced_ratio
 
 
